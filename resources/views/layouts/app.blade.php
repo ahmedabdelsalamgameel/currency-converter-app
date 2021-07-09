88ab55bd5446b9ca4acd157f0af3dd61ceec5dcc
Ahmed <ahmedabdelsalamgameel1032@gmail.com>
2021-07-10 01:48:10 +0200

    apdated files

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <link rel="stylesheet" href="//use.fontawesome.com/releases/v5.0.7/css/all.css">
+    <title>Converter</title>
+</head>
+<body class="bg-gray-300 h-screen leading-none font-sans">
+    @yield('content')
+</body>
+</html>
